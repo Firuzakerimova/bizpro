a26624698cae43432bed12c7c9c00908efb4e882
Firuzakerimova <bd7mbuwz1@code.edu.az>
2024-01-15 02:13:00 +0400

    bizpro

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <h1 className="header-h1">BizPro</h1>
+        <ul>
+          <li>HOME</li>
+          <li>ABOUT</li>
+          <li>SERVICES</li>
+          <li>PORTFOLIO</li>
+          <li>TEAM</li>
+          <li>SKILL</li>
+          <li>CLIENTS</li>
+          <li>PRICING</li>
+          <li>BLOG</li>
+          <li>CONTACT</li>
+        </ul>
+        <div className="body">
+          <h1 className="body-tittle">HELLO WE ARE BIZPRO</h1>
+          <h className="body-tittle">SUB HEAD,MOTTO OR MISSION SUBTITLE</h>
+          <a href="https://preview.colorlib.com/theme/bizpro/contact-us.html">
+            SEE OUR PROJECTS
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
